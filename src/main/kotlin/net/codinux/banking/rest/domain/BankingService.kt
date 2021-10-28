97c9f2d230bbd5c054e5bfe7e65b6039c9459b66
dankito <git@dankito.net>
2021-10-28 23:54:03 +0200

    Removed finTsServerAddress from BankCredentials

--- src/main/kotlin/net/codinux/banking/rest/domain/BankingService.kt
+++ src/main/kotlin/net/codinux/banking/rest/domain/BankingService.kt
@@ -31,16 +31,16 @@ class BankingService {
     return hbci4jBankingClient(bank)
   }
 
-  private fun mapToBankData(accessData: BankCredentials): Pair<BankData, String?> {
-    val bankSearchResult = bankFinder.findBankByBankCode(accessData.bankCode)
-    val fintsServerAddress = accessData.finTsServerAddress ?: bankSearchResult.firstOrNull { it.pinTanAddress != null }?.pinTanAddress
+  private fun mapToBankData(credentials: BankCredentials): Pair<BankData, String?> {
+    val bankSearchResult = bankFinder.findBankByBankCode(credentials.bankCode)
+    val fintsServerAddress = bankSearchResult.firstOrNull { it.pinTanAddress != null }?.pinTanAddress
     val potentialBankInfo = bankSearchResult.firstOrNull()
 
-    val bank = BankData(accessData.bankCode, accessData.loginName, accessData.password, fintsServerAddress ?: "",
+    val bank = BankData(credentials.bankCode, credentials.loginName, credentials.password, fintsServerAddress ?: "",
       potentialBankInfo?.name ?: "", potentialBankInfo?.bic ?: "")
 
     if (fintsServerAddress == null) {
-      val errorMessage = if (bankSearchResult.isEmpty()) "No bank found for bank code '${accessData.bankCode}'" else "Bank '${bankSearchResult.firstOrNull()?.name} does not support FinTS 3.0"
+      val errorMessage = if (bankSearchResult.isEmpty()) "No bank found for bank code '${credentials.bankCode}'" else "Bank '${bankSearchResult.firstOrNull()?.name} does not support FinTS 3.0"
 
       return Pair(bank, errorMessage)
     }
--- src/main/kotlin/net/codinux/banking/rest/domain/model/BankCredentials.kt
+++ src/main/kotlin/net/codinux/banking/rest/domain/model/BankCredentials.kt
@@ -4,8 +4,7 @@ package net.codinux.banking.rest.domain.model
 open class BankCredentials(
     open val bankCode: String,
     open val loginName: String,
-    open val password: String,
-    open val finTsServerAddress: String? = null
+    open val password: String
 ) {
 
     internal constructor() : this("", "", "") // for object deserializers
