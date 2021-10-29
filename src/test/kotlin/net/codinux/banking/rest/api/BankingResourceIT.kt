4254fbd2315b2926eabdaa0ef1aa92f80040b2d0
dankito <git@dankito.net>
2021-10-29 03:01:03 +0200

    Implemented extracting response data

--- src/test/kotlin/net/codinux/banking/rest/api/BankingResourceIT.kt
+++ src/test/kotlin/net/codinux/banking/rest/api/BankingResourceIT.kt
@@ -1,15 +1,13 @@
 package net.codinux.banking.rest.api
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import io.restassured.response.Response
-import net.codinux.banking.rest.domain.model.BankCredentials
+import net.codinux.banking.rest.domain.model.*
+import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.CoreMatchers.*
 import org.junit.jupiter.api.Test
-
-import javax.inject.Inject
 
 @QuarkusTest
 class BankingResourceIT {
@@ -23,34 +21,38 @@ class BankingResourceIT {
   }
 
 
-  @Inject
-  internal lateinit var mapper: ObjectMapper
-
-
   @Test
   fun getAccountData() {
 
-    post("account", BankCredentials(bankCode, loginName, password))
-      .then()
-        .log().all()
-        .statusCode(200)
-        .contentType(ContentType.JSON)
-        .body("successful", `is`(true))
-        .body("error", nullValue())
-        .body("data", not(nullValue()))
+    val bankData = postAndValidate("account", BankCredentials(bankCode, loginName, password), BankData::class.java)
+
+    assertThat(bankData.bankCode).isEqualTo(bankCode)
+    assertThat(bankData.loginName).isEqualTo(loginName)
+    assertThat(bankData.accounts).isNotEmpty
+    assertThat(bankData.supportedTanMethods).isNotEmpty
   }
 
 
   private fun post(endpoint: String, body: Any): Response {
     return given()
             .contentType(ContentType.JSON)
-            .body(serialize(body))
+            .body(body)
           .`when`()
             .post("/banking/v1/" + endpoint)
   }
 
-  private fun serialize(body: Any): String {
-    return mapper.writeValueAsString(body)
+  private fun <T> postAndValidate(endpoint: String, body: Any, responseClass: Class<T>): T {
+    val response = post(endpoint, body)
+
+    response.then()
+      .log().all()
+      .statusCode(200)
+      .contentType(ContentType.JSON)
+      .body("successful", `is`(true))
+      .body("error", nullValue())
+      .body("data", not(nullValue()))
+
+    return response.jsonPath().getObject("data", responseClass)
   }
 
 }
