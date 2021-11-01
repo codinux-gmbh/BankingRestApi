f0fba0c3383451e20f4557eaf403e9fab03402a0
dankito <git@dankito.net>
2021-11-01 19:38:19 +0100

    Added comments for types

--- src/main/kotlin/net/codinux/banking/rest/domain/model/tan/TanMediumType.kt
+++ src/main/kotlin/net/codinux/banking/rest/domain/model/tan/TanMediumType.kt
@@ -3,10 +3,19 @@ package net.codinux.banking.rest.domain.model.tan
 
 enum class TanMediumType {
 
+  /**
+   * All other TAN media, like AppTan.
+   */
   Generic,
 
+  /**
+   * If I'm not wrong MobilePhone is only used for SmsTan.
+   */
   MobilePhone,
 
+  /**
+   * Mostly used for chipTan.
+   */
   TanGenerator
 
 }
