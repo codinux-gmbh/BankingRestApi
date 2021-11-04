1a2ab42e413b4d5cb37591f6b330a2c198cdfab7
dankito <git@dankito.net>
2021-11-04 18:33:00 +0100

    Removed unused RetrievedAccountTransactions

--- src/main/kotlin/net/codinux/banking/rest/domain/model/RetrievedAccountTransactions.kt
+++
@@ -1,9 +0,0 @@
-package net.codinux.banking.rest.domain.model
-
-import java.math.BigDecimal
-
-
-class RetrievedAccountTransactions(
-  val transactions: List<AccountTransaction>,
-  val balance: BigDecimal? = null
-)
